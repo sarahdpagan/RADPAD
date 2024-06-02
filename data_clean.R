ab797fec2c636ce5639feff211ee6d978c69e307
sarahpagan <sdpagan@ncsu.edu>
2024-06-02 17:01:19 -0400

    mixed mod

--- data_clean.R
+++ data_clean.R
@@ -1,17 +1,22 @@
 library(tidyverse)
+library(readxl)
 
-radpad <- read_csv("RADPAD DATA Stats team 5_28_24_FINAL.xlsx - Sheet1.csv", skip = 1)
-str(radpad)
+radpad <- read_excel("RADPAD DATA Stats team 5_28_24_FINAL.xlsx", skip = 1) |>
+  select(-Breed, -`Sex (FI, FS, MI, MN)`)
 
 long <- radpad |>
   select(-Patient...20, -`DAP Total (Gycm2)`) |>
+  mutate(ID = 1:200) |>
   mutate(`Resident 1` = as.character(`Resident 1`)) |>
   pivot_longer(Faculty:Anesthesia,
                names_to = "Occupation",
                values_to = "Dose") |>
   mutate(across(where(is.character), ~na_if(., "n/a"))) |>
+  mutate(across(where(is.character), ~na_if(., "n /a"))) |>
   mutate(across(where(is.character), ~na_if(., "NB"))) |>
-  drop_na()
+  drop_na() |>
+  mutate(Dose = as.numeric(Dose))
 
 write_csv(long, "radpad.csv")
+
 
